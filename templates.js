2fc35e12a78fa5f9c6f5239f45fe5414a8174742
Dmytro-Salenko <d.salenko@gmail.com>
2025-07-14 22:04:44 +0200

    Update templates.js

--- templates.js
+++ templates.js
@@ -4,5 +4,14 @@ const envelopeTemplates = [
     title: "Minimalist wedding envelope #1",
     image: "/images/template1-5x7.jpg"
   },
-  // другие шаблоны…
+  {
+    id: "b6b43f09460ce3d3eca5e3b27e06db60",
+    title: "Minimalist wedding envelope #2",
+    image: "/images/template2-5x7.jpg"
+  },
+  {
+    id: "3bc48c9deef234ae9bbfaeb8dc7f4ef2",
+    title: "Minimalist wedding envelope #3",
+    image: "/images/template3-5x7.jpg"
+  }
 ];
